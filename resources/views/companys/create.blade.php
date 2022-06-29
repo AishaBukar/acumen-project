42251997910dea41c984f5498f32c51d0e68decc
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2022-06-29 10:49:07 +0100

    modifications

--- resources/views/companys/create.blade.php
+++ resources/views/companys/create.blade.php
@@ -1,53 +1,71 @@
 @extends('companys.layout')
   
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add New Company</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('companys.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
-   
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<form action="{{ route('companys.store') }}" method="POST">
-    @csrf
-  
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="name">
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Add Company Form</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    </head>
+    <body>
+        <div class="container ">
+            <div class="row">
+                <div class="col-lg-12 margin-tb">
+                    <div class="pull-left mb-2">
+                        <h2>Add Company</h2>
+                    </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('companys.index') }}"> Back</a>
+                </div>
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email</strong>
-                <textarea class="form-control" style="height:150px" name="email" placeholder="email"></textarea>
+             @if(session('status'))
+             <div class="alert alert-success mb-1 mt-1">
+                {{ session('status') }}
             </div>
+            @endif
+            <form action="{{ route('companys.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Company Name:</strong>
+                            <input type="text" name="name" class="form-control" placeholder="Company Name">
+                            @error('name')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Company Email:</strong>
+                            <input type="email" name="email" class="form-control" placeholder="Company Email">
+                            @error('email')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Company Website:</strong>
+                            <input type="text" name="website" class="form-control" placeholder="Company website">
+                            @error('website')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+        
+        
+            </form>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Website</strong>
-                <textarea class="form-control" style="height:150px" name="website" placeholder="website"></textarea>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
-   
-</form>
+    </body>
+    </html>
+
+
+
 @endsection
