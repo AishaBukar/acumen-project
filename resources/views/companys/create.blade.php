0d63e1786a003b998fc74e9813bb505e971ef5a7
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2022-06-29 13:47:42 +0100

    create

--- resources/views/companys/create.blade.php
+++ resources/views/companys/create.blade.php
@@ -1,74 +1,62 @@
-
-
-
 @extends('companys.layout')
    
 @section('content')
-<div class="container ">
-            <div class="row">
-                <div class="col-lg-12 margin-tb">
-                    <div class="pull-left ">
-                        <h2>Add Company Form</h2>
-                    </div>
-                <div class="pull-right">
-                    <a class="btn btn-primary" href="{{ route('companys.index') }}"> Back</a>
-                </div>
-                </div>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Add Company Form</h2>
             </div>
-             @if(session('status'))
-             <div class="alert alert-success mb-1 mt-1">
-                {{ session('status') }}
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('companys.index') }}"> Back</a>
             </div>
-            @endif
         </div>
-            <form action="{{ route('companys.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Company Name:</strong>
-                            <input type="text" name="name" class="form-control" placeholder="Company Name">
-                            @error('name')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Company Email:</strong>
-                            <input type="email" name="email" class="form-control" placeholder="Company Email">
-                            @error('email')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Company Website:</strong>
-                            <input type="text" name="website" class="form-control" placeholder="Company website">
-                            <!-- @error('website')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                            @enderror -->
-                        </div>
-
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="form-group">
-                            <strong>Company Logo:</strong>
-                            <input type="file" name="logo" class="form-control" placeholder="logo">
-                            @error('logo')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                    </div>
-                    
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+  
+    <form action="{{ route('companys.store') }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $company->name }}" class="form-control" placeholder="Name">
                 </div>
-        
-        
-            </form>
-</div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <textarea class="form-control" style="height:150px" name="email" placeholder="email">{{ $company->email }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Website:</strong>
+                    <textarea class="form-control" style="height:150px" name="website" placeholder="website">{{ $company->website }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Logo:</strong>
+                    <input type="file" name="logo" value="{{ $company->logo }}" class="form-control" placeholder="logo">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+    </div>
+</form>
 @endsection
+
 
