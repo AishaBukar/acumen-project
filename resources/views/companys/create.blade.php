157d17c3416bc48ecee603b9b655b18e3bfcafa6
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2022-06-29 13:25:25 +0100

    create

--- resources/views/companys/create.blade.php
+++ resources/views/companys/create.blade.php
@@ -12,7 +12,7 @@
         <div class="container ">
             <div class="row">
                 <div class="col-lg-12 margin-tb">
-                    <div class="pull-left mb-2">
+                    <div class="pull-left ">
                         <h2>Add Company</h2>
                     </div>
                 <div class="pull-right">
